38722b97b9b7f5addcbfd396b4b7d0c29e121427
Konstantin Shulga <mrkonstantinsh@gmail.com>
2023-08-22 09:31:14 +0300

    refactor: add spaces between params in the InlineData attribute

--- tests/SortingAlgorithms.UnitTests/InsertionSortTests.cs
+++ tests/SortingAlgorithms.UnitTests/InsertionSortTests.cs
@@ -3,9 +3,9 @@ namespace SortingAlgorithms.UnitTests;
 public class InsertionSortTests
 {
     [Theory]
-    [InlineData(new[] { 5, 4, 3, 2, 1, 0 },new[] { 0, 1, 2, 3, 4, 5 })]
-    [InlineData(new[] { 0, -1, -2, -3, -4, -5 },new[] { -5, -4, -3, -2, -1, 0 })]
-    [InlineData(new[] { -10, 4, 2, -5, 1, 0 },new[] { -10, -5, 0, 1, 2, 4 })]
+    [InlineData(new[] { 5, 4, 3, 2, 1, 0 }, new[] { 0, 1, 2, 3, 4, 5 })]
+    [InlineData(new[] { 0, -1, -2, -3, -4, -5 }, new[] { -5, -4, -3, -2, -1, 0 })]
+    [InlineData(new[] { -10, 4, 2, -5, 1, 0 }, new[] { -10, -5, 0, 1, 2, 4 })]
     public void Sorting_in_ascending_order_must_be_correct(int[] arrayToSort, int[] expectedSortedArray)
     {
         var sortedArray = InsertionSort.SortAscending(arrayToSort);
@@ -14,9 +14,9 @@ public class InsertionSortTests
     }
     
     [Theory]
-    [InlineData(new[] { 0, 1, 2, 3, 4, 5 },new[] { 5, 4, 3, 2, 1, 0 })]
-    [InlineData(new[] { -5, -4, -3, -2, -1, 0 },new[] { 0, -1, -2, -3, -4, -5 })]
-    [InlineData(new[] { -10, 4, 2, -5, 1, 0 },new[] { 4, 2, 1, 0, -5, -10})]
+    [InlineData(new[] { 0, 1, 2, 3, 4, 5 }, new[] { 5, 4, 3, 2, 1, 0 })]
+    [InlineData(new[] { -5, -4, -3, -2, -1, 0 }, new[] { 0, -1, -2, -3, -4, -5 })]
+    [InlineData(new[] { -10, 4, 2, -5, 1, 0 }, new[] { 4, 2, 1, 0, -5, -10})]
     public void Sorting_in_descending_order_must_be_correct(int[] arrayToSort, int[] expectedSortedArray)
     {
         var sortedArray = InsertionSort.SortAscending(arrayToSort);
