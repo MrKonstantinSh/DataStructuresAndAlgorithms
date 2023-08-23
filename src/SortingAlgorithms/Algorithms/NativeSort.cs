0e00a4384595a8db4a7273d632f80f87fa38be6a
Konstantin Shulga <k.shulga@softteco.com>
2023-08-23 10:19:14 +0300

    feat: implement native for C# sorting

---
+++ src/SortingAlgorithms/Algorithms/NativeSort.cs
@@ -0,0 +1,26 @@
+﻿namespace SortingAlgorithms.Algorithms;
+
+public static class NativeSort
+{
+    public static int[] SortAscending(int[] arrayToSort)
+    {
+        var resultArray = new int[arrayToSort.Length];
+        Array.Copy(arrayToSort, resultArray, arrayToSort.Length);
+        
+        // Algorithm.
+        Array.Sort(resultArray);
+
+        return resultArray;
+    }
+    
+    public static int[] SortDescending(int[] arrayToSort)
+    {
+        var resultArray = new int[arrayToSort.Length];
+        Array.Copy(arrayToSort, resultArray, arrayToSort.Length);
+        
+        // Algorithm.
+        Array.Sort(resultArray, (first, second) => second.CompareTo(first));
+
+        return resultArray;
+    }
+}
